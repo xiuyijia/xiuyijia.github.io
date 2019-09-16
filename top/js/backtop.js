4d873458d9916a68d9a1ba1d3bf21aad7814bcad
unknown <https://github.com/ranran4082391/Weakly-Supervised-Model.git>
2019-09-16 12:55:27 +0800

    message

--- top/js/backtop.js
+++ top/js/backtop.js
@@ -20,7 +20,7 @@ chinaz.prototype = {
 		$(window).bind("scroll",function() {
             var d = $(document).scrollTop(),
             e = $(window).height();
-            0 < d ? $(".cbbfixed").css("bottom", "-1400px") : $(".cbbfixed").css("bottom", "-200px");
+            0 < d ? $(".cbbfixed").css("bottom", "-1900px") : $(".cbbfixed").css("bottom", "-200px");
 			clearTimeout(timmer);
 			timmer = setTimeout(function() {
                 clearTimeout(timmer)
