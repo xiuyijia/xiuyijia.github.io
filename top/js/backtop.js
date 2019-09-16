643d4c708e80fe685eec87a5de1c751952205f04
unknown <https://github.com/ranran4082391/Weakly-Supervised-Model.git>
2019-09-16 11:39:28 +0800

    message

---
+++ top/js/backtop.js
@@ -0,0 +1,32 @@
+//javascript Document
+function chinaz(){
+	this.init();
+}
+chinaz.prototype = {
+	constructor: chinaz,
+	init: function(){		
+		this._initBackTop();
+	},	
+	_initBackTop: function(){
+
+		
+		$(".cbbfixed").click(function(){
+			$("html, body").animate({
+				scrollTop: 0
+			}, 120);
+		});
+
+		var timmer = null;
+		$(window).bind("scroll",function() {
+            var d = $(document).scrollTop(),
+            e = $(window).height();
+            0 < d ? $(".cbbfixed").css("bottom", "-1800px") : $(".cbbfixed").css("bottom", "-200px");
+			clearTimeout(timmer);
+			timmer = setTimeout(function() {
+                clearTimeout(timmer)
+            },100);
+	   });
+	}
+	
+}
+var chinaz = new chinaz();
